d54b0e56e6e2a7e658b2cbdef5eb292d3812a8ee
hasn1 <sh5430678@gmail.com>
2026-01-16 23:22:07 +0300

    add backend files

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use("/api/delver", delvreRoutes);
 // 8️⃣ اتصال MongoDB
 // ------------------------
 mongoose
-  .connect("mongodb://127.0.0.1:27017/testdb")
+  .connect("mongodb://localhost/:27017/testdb")
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ MongoDB Error:", err));
 
